ca9a768124ea113e0f3324dfb34c91fc0c540b47
ohadhell <81020823+ohadhell@users.noreply.github.com>
2023-01-06 12:31:12 +0200

    9 tests

--- test/ReviewSystem.js
+++ test/ReviewSystem.js
@@ -30,7 +30,6 @@ contract('ReviewSystem', (accounts) => {
       
         // Check that the number of reviews is 1
         const numReviews = await reviewSystem.getNumReviews(business1);
-        console.log("Number of reviews: ", numReviews);
         assert.equal(numReviews['words'][0], 2);
       });
 
@@ -79,7 +78,6 @@ contract('ReviewSystem', (accounts) => {
               await reviewSystem.addReview(0, 'Invalid rating', business1, reviewer1);
               assert.fail('Expected addReview to revert with an error');
             } catch (error) {
-                console.log("Error: ", error.message);
               assert.ok(error.message.startsWith('VM Exception while processing transaction: revert Rating must be between 1 and 5'));
             }
           
@@ -92,5 +90,54 @@ contract('ReviewSystem', (accounts) => {
             }
           });
 
+          it('should not allow rewarding a user for a review that does not exist', async () => {
+            const reviewSystem = await ReviewSystemJson.deployed();
+            const rewardAmount = 100; // arbitrary reward amount in wei
+            const nonExistentReviewId = 99; // arbitrary review ID that does not exist
+            try {
+              await reviewSystem.rewardUser(nonExistentReviewId, business1, { value: rewardAmount, from: business1 });
+              assert.fail('Expected revert not received');
+            } catch (error) {
+              assert.ok(error.message.startsWith('VM Exception while processing transaction: revert'), 'Unexpected error: ' + error.message);
+            }
+          });
+
+          it('should get the correct review data for a given review ID', async () => {
+            const reviewSystem = await ReviewSystemJson.deployed();
+          
+            // Add a review
+            const review = await reviewSystem.addReview(5, 'Great business!', business1, reviewer1);
+          
+            // Get review data using review ID
+            const reviewCount = await reviewSystem.reviewCount();
+            const reviewId = reviewCount['words'][0];
+            const result = await reviewSystem.getReview(reviewId);
+          
+            // Check that returned data matches expected values
+            assert.equal(result[0]['words'][0], reviewId);
+            assert.equal(result[1]['words'][0], 5);
+            assert.equal(result[2], 'Great business!');
+            assert.equal(result[3], business1);
+            assert.equal(result[4], reviewer1);
+          });
+          
+          it('should not allow a user to be rewarded multiple times', async () => {
+            const reviewSystem = await ReviewSystemJson.deployed();
+            const review = await reviewSystem.addReview(rating, reviewString, business1, reviewer1, { from: business1 });
+            // Get review data using review ID
+            const reviewCount = await reviewSystem.reviewCount();
+            const reviewId = reviewCount['words'][0];
+            await reviewSystem.rewardUser(reviewId, business1, { value: 1, from: business1 });
+            // Try to reward user again
+            try {
+              await reviewSystem.rewardUser(reviewId, business1, { value: 1, from: business1 });
+              // If the above line does not throw an error, the test has failed
+              assert.fail('User was able to be rewarded multiple times');
+            } catch (err) {
+              // Check that the error message is correct
+              assert.ok(err.message.startsWith('VM Exception while processing transaction: revert User has already been rewarded'));
+            }
+          });
+          
 
 });
